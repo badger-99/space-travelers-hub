4ea654d45e5ea60dc2e38c5354c1d2cf01881973
Alfred Peter <alpeter91@gmail.com>
2023-08-07 22:24:00 +0300

    Dispatch api call

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketSlice';
+
 const Rockets = () => {
   const {
     rocketArray, isLoading, error,
   } = useSelector((store) => store.rockets);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
 
   return (
     <h1>Rockets</h1>
