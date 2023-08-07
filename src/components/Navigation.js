8b363231cb51649d667420585356dd53aafa1804
Alfred Peter <alpeter91@gmail.com>
2023-08-07 14:29:15 +0300

    Add nav component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,12 @@
+import logo from '../assets/logo.png';
+
 const Navigation = () => {
+  const heading = "Space Traveler's Hub";
   return (
-    <nav>Navigation</nav>
-  )
-}
-export default Navigation
+    <nav className="row">
+      <img src={logo} alt="planet logo" style={{ width: '4rem', marginRight: '2rem' }} />
+      <h1>{heading}</h1>
+    </nav>
+  );
+};
+export default Navigation;
