e1bc1d6e2939603ce8ea863686f56081df30fe36
Alfred Peter <alpeter91@gmail.com>
2023-08-07 18:13:25 +0300

    Render rockets page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Placeholder from './components/placeholder';
 import MyProfile from './components/MyProfile';
+import Rockets from './components/Rockets';
 
 const App = () => (
   <>
     <Navigation />
     <Routes>
-      <Route path="/" element={<Placeholder />} />
+      <Route path="/" element={<Rockets />} />
+      <Route path="/placeholder" element={<Placeholder />} />
       <Route path="/my_profile" element={<MyProfile />} />
     </Routes>
   </>
