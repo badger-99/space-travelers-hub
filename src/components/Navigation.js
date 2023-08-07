22d849edb2cf1653f94190038caa154c8c688fb3
Alfred Peter <alpeter91@gmail.com>
2023-08-07 15:13:30 +0300

    Add navlink elements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navigation = () => {
@@ -6,6 +7,14 @@ const Navigation = () => {
     <nav className="row">
       <img src={logo} alt="planet logo" style={{ width: '4rem', marginRight: '2rem' }} />
       <h1>{heading}</h1>
+      <ul className="navLinks row">
+        <li>
+          <NavLink className="link" to="/"> placeholder</NavLink>
+        </li>
+        <li>
+          <NavLink className="link" to="/my_profile"> My Profile</NavLink>
+        </li>
+      </ul>
     </nav>
   );
 };
