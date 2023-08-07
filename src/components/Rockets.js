3bd50a752d17737ec093abfb9908e798754b9e3a
Alfred Peter <alpeter91@gmail.com>
2023-08-07 22:23:21 +0300

    Get rocket data from store

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,13 @@
-const Rockets = () => (
-  <h1>Rockets</h1>
-);
+import { useDispatch, useSelector } from 'react-redux';
+const Rockets = () => {
+  const {
+    rocketArray, isLoading, error,
+  } = useSelector((store) => store.rockets);
+  const dispatch = useDispatch();
+
+  return (
+    <h1>Rockets</h1>
+  );
+};
 
 export default Rockets;
