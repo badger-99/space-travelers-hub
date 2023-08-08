b0723f99dab685ea2b90fce7c61673746d645e34
Alfred Peter <alpeter91@gmail.com>
2023-08-08 17:02:02 +0300

    Implement toggle reservation logic for rockets

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,21 +1,26 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { toggleReservation } from '../redux/rockets/rocketSlice';
 
 const Rocket = ({
   id, name, description, image,
-}) => (
-  <div className="rocket flexRow">
-    <div className="rocketPic">
-      <img src={image} alt={name} />
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="rocket flexRow">
+      <div className="rocketPic">
+        <img src={image} alt={name} />
+      </div>
+      <div className="details flexCol">
+        <h4>{name}</h4>
+        <p>{description}</p>
+        <button type="button" id={id} onClick={() => dispatch(toggleReservation(id))}>
+          Reserve Rocket
+        </button>
+      </div>
     </div>
-    <div className="details flexCol">
-      <h4>{name}</h4>
-      <p>{description}</p>
-      <button type="button" id={id}>
-        Reserve Rocket
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 Rocket.propTypes = {
   id: PropTypes.string.isRequired,
