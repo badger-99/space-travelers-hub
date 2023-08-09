8c0978adee32f2bb80dbb5b95cc4837fd3e0eea3
Alfred Peter <alpeter91@gmail.com>
2023-08-10 02:29:57 +0300

    Add test for Missions page

---
+++ src/pages/Missions.test.jsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Missions from './Missions';
+import { store } from '../redux/store';
+
+test('snapshot of Missions page', () => {
+  const missionsList = render(
+    <Provider store={store}>
+      <Missions />
+    </Provider>
+  );
+
+  expect(missionsList).toMatchSnapshot();
+});
